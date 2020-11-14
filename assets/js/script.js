5cc419cbb6bfdc1427beddfce41dcdce094a0fe5
Mohamed Ghazi <m.mahmoud@uplift.plus>
2020-11-15 01:50:10 +0200

    Update Functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,40 @@
+const increment = document.getElementsByClassName('inc');
+const decrement = document.getElementsByClassName('dec');
+const number = document.getElementsByClassName('num');
+const select = document.getElementById('select-item');
+const list = document.getElementById('options-list');
+const options = document.getElementsByClassName('option');
+let z = 0; let y = 0;
 
-$('.pop-up').magnificPopup({
-    type: 'iframe'
-});
-/*
-$('.mile-counter').counterUp ({
-    delay:10,
-    counter:1000
-});
-*/
-var increment = document.getElementsByClassName('inc');
-var decrement = document.getElementsByClassName('dec');
-var number = document.getElementsByClassName('num');
-var z = 0;
-var y = 0;
-for ( i =0 ; i < increment.length ; i ++) {
+for (let i =0 ; i < increment.length ; i ++) {
      increment[i].addEventListener('click' , function(){
-        var prevsibling = this.previousElementSibling;
-         z+=1;
-         prevsibling.value = z;
+        let prevSibling = this.previousElementSibling;
+        z = Number(prevSibling.value);
+        z+=1;
+        prevSibling.value = z;
      });
-};
+}
 
-for ( j =0 ; j < decrement.length ; j ++) {
+for (let j =0 ; j < decrement.length ; j ++) {
     decrement[j].addEventListener('click' , function(){
-       var nextsibling = this.nextElementSibling;
+        let prevSibling = this.nextElementSibling;
+        y = Number(prevSibling.value);
         y-=1;
-        nextsibling.value = y;
+        prevSibling.value = y;
     });
-};
-
-var select = document.getElementById('select-item');
-var list = document.getElementById('options-list');
+}
 
 select.addEventListener('click',function(){
-    if(list.style.display === 'none') {
-      list.style.display ='block';
-    }
-    else {
-        list.style.display = 'none';
+    list.classList.toggle('display-block');
+    for (let j = 0 ; j <= options.length ; j ++) {
+        options[j].addEventListener('click', function(){
+            select.innerHTML = this.innerHTML;
+            list.classList.remove('display-block');
+        });
     }
 });
 
-var options = document.getElementsByClassName('option');
-
-for ( j = 0 ; j <= options.length ; j ++) {
-    options[j].addEventListener('click', function(){
-
-        select.innerHTML = this.innerHTML;
-    });
-};
-
-
-
-
+$('.mile-counter').counterUp({
+    delay: 10,
+    time: 1000
+});;
